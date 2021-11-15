8d55e476a80783e227d841353eed237d8f3ed1cd
DashaTimoshenko <43323943+DashaTimoshenko@users.noreply.github.com>
2021-11-15 14:17:14 +0200

    Update models.py

--- models.py
+++ models.py
@@ -26,9 +26,6 @@ class Author(db.Model):
 
     #editions = relationship('EditionInf', secondary='edition_author')
 
-    def __repr__(self):
-        return "<Author (author_id='%s'; author_name='%s'; author_surname='%s'; author_middle_name='%s')>" % (
-            self.author_id, self.author_name, self.author_surname, self.author_middle_name)
 
 
 class EditionInf(db.Model):
@@ -43,9 +40,6 @@ class EditionInf(db.Model):
 
     # edition_count = relationship('EditionCount', uselist=False)
 
-    def __repr__(self):
-        return "<EditionInf (edition_id='%s'; book_title='%s'; edition_year='%d')>" % (
-            self.edition_id, self.book_title, self.edition_year)
 
 
 class EditionCount(EditionInf):
@@ -61,15 +55,6 @@ class Genre(db.Model):
 
     genre_id = Column(Integer, db.Sequence('genre_genre_id_seq'), primary_key=True)
     genre = Column(Text, nullable=False, unique=True)
-
-
-class Order(db.Model):
-    __tablename__ = 'orders'
-
-    order_id = Column(Integer, db.Sequence('orders_order_id_seq'), primary_key=True)
-    booking_date = Column(Date, nullable=False)
-    issue_date = Column(Date)
-    is_canceled = Column(Boolean, nullable=False, server_default=text("false"))
 
 
 class Permission(db.Model):
@@ -112,9 +97,6 @@ class UserInf(db.Model):
     role = relationship("Role", secondary='user_role', uselist=False)
     status = relationship("Status", secondary='user_status', uselist=False)
 
-    def __repr__(self):
-        return "<UserInf (user_login='%s'; user_password='%s'; user_name='%s'; surname='%s'; middle_name='%s'; user_id='%d')>" % (
-            self.user_id, self.user_password, self.user_name, self.surname, self.middle_name, self.user_id)
 
 
 class Book(db.Model):
@@ -132,56 +114,57 @@ class Book(db.Model):
 
 t_edition_author = Table(
     'edition_author', metadata,
-    Column('edition_id', ForeignKey('edition_inf.edition_id', ondelete='RESTRICT', onupdate='RESTRICT'),
-           primary_key=True, nullable=False),
-    Column('author_id', ForeignKey('author.author_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False)
+    Column('edition_id', ForeignKey('edition_inf.edition_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False),
+    Column('author_id', ForeignKey('author.author_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
 )
+
 
 t_edition_genre = Table(
     'edition_genre', metadata,
-    Column('edition_id', ForeignKey('edition_inf.edition_id', ondelete='RESTRICT', onupdate='RESTRICT'),
-           primary_key=True, nullable=False),
-    Column('genre_id', ForeignKey('genre.genre_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False)
+    Column('edition_id', ForeignKey('edition_inf.edition_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False),
+    Column('genre_id', ForeignKey('genre.genre_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
 )
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    order_id = Column(Integer, primary_key=True, server_default=text("nextval('orders_order_id_seq'::regclass)"))
+    user_id = Column(ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), nullable=False)
+    booking_date = Column(Date, nullable=False)
+    issue_date = Column(Date)
+    is_canceled = Column(Boolean, nullable=False, server_default=text("false"))
+
+    user = relationship('UserInf')
+
 
 t_role_permission = Table(
     'role_permission', metadata,
-    Column('role_id', ForeignKey('roles.role_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False),
-    Column('permission_id', ForeignKey('permissions.permission_id', ondelete='RESTRICT', onupdate='RESTRICT'),
-           primary_key=True, nullable=False)
+    Column('role_id', ForeignKey('roles.role_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False),
+    Column('permission_id', ForeignKey('permissions.permission_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
 )
+
 
 t_user_role = Table(
     'user_role', metadata,
-    Column('role_id', ForeignKey('roles.role_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False),
-    Column('user_id', ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False)
+    Column('role_id', ForeignKey('roles.role_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False),
+    Column('user_id', ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
 )
+
 
 t_user_status = Table(
     'user_status', metadata,
-    Column('user_id', ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False),
-    Column('status_id', ForeignKey('status.status_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-           nullable=False)
+    Column('user_id', ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False),
+    Column('status_id', ForeignKey('status.status_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
 )
 
 
 class OrderBook(db.Model):
     __tablename__ = 'order_book'
 
-    user_id = Column(ForeignKey('user_inf.user_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-                     nullable=False)
-    book_id = Column(ForeignKey('book.book_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-                     nullable=False)
-    order_id = Column(ForeignKey('orders.order_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True,
-                      nullable=False)
+    book_id = Column(ForeignKey('book.book_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
+    order_id = Column(ForeignKey('orders.order_id', ondelete='RESTRICT', onupdate='RESTRICT'), primary_key=True, nullable=False)
     return_date = Column(Date)
 
     book = relationship('Book')
     order = relationship('Order')
-    user = relationship('UserInf')
