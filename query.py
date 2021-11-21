a5e7f0088f797ca59ce9f98980dbaff394c883fb
DariaGera <43523466+DariaGera@users.noreply.github.com>
2021-11-21 18:08:29 +0200

    Update query.py

--- query.py
+++ query.py
@@ -4,14 +4,30 @@ from models import *
 def get_edition_by_book_id(book_id):
     return Book.query.filter_by(book_id=book_id).first().edition
 
+
+def get_all_books_by_edition_id(edition_id):
+    return Book.query.filter_by(edition_id=edition_id).all()
+
+
 def get_edition_count_obj(edition_id):
     return EditionCount.query.filter_by(edition_id=edition_id).first()
+
+
+def get_edition_info_obj(edition_id):
+    return EditionInf.query.filter_by(edition_id=edition_id).first()
+
+
+def get_all_orders_by_user_id(user_id):
+    return Order.query.filter_by(user_id=user_id).all()
+
 
 def get_all_books_by_order_id(order_id):
     return OrderBook.query.filter_by(order_id=order_id).all()
 
+
 def get_user_by_id(user_id):
     return UserInf.query.filter_by(user_id=user_id).first()
+
 
 def get_status_name(user):
     return user.status.status_name
