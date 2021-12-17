abf54f0755358d4623981e6e968e58566721b454
Hlib Yudin <43316758+hlib-yudin@users.noreply.github.com>
2021-12-17 19:39:55 +0200

    changed config.py for deployment

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ def configurate(app):
     # development 
     #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1111@localhost:5432/postgres"
     #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:040801@localhost:5432/library_db"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/library_db"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/library_db"
     #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:5432/library_db"
 
-    app.config['SECRET_KEY'] = 'kfgvTKF_GgvgvfCFmg6yu6-VGHVgfvgGGhH_Szz245m_kkPh9qk'
+    #app.config['SECRET_KEY'] = 'kfgvTKF_GgvgvfCFmg6yu6-VGHVgfvgGGhH_Szz245m_kkPh9qk'
     
 
     # production
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-    #app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 
     app.config["SESSION_PERMANENT"] = False
