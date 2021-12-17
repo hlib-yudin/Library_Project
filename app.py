5f7748f664ac4f0da492fad5082a449de878f495
DariaGera <43523466+DariaGera@users.noreply.github.com>
2021-12-17 10:02:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, make_response, render_template, url_for, request, redirect, jsonify,json, flash, session
-from flask_session import Session
-# from config import Config
-from dateutil.relativedelta import *
-import json
-import os
-from config import configurate
 from graphіcs import *
-#
-app = Flask(__name__, template_folder='boostrap/Pages')
-configurate(app)
-# app.config.from_object(Config)
-
-
-# SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
-
-
-Session(app)
-
-db = SQLAlchemy(app)
-db.init_app(app)
-
 
 
 # html routing
